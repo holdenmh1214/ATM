06318b579ea162cb75738997e9f23d2dfdf07065
holdenhughes <holdenmh121489@gmail.com>
2015-10-09 15:42:13 -0400

    Included loop, hashmap, accounts, new balance after withdrawal

--- src/ATM.java
+++ src/ATM.java
@@ -6,39 +6,58 @@ import java.util.*;
 
 public class ATM {
     public static void run() throws Exception {
-        System.out.println("Type Name");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        int balance = 100;
+        HashMap<String, Double> accounts = new HashMap();
+        while (true){
+            System.out.println("Type Name");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
 
-        if (name.length() == 0) {
-            throw new Exception("Please try again");
-        }
 
-        System.out.println("1. Check my balance");
-        System.out.println("2. Withdraw Funds");
-        System.out.println("3. Cancel");
+            if (name.length() == 0) {
+                throw new Exception("Please try again");
+            } else if (!accounts.containsKey(name)) {
+                System.out.println("Account name not found");
+                System.out.println("Would you like to create new account? [y]/[n]");
+                String choice = scanner.nextLine();
+                if (choice.equals("y")) {
+                    accounts.put(name, 100.0);
+                }else {
+                    System.out.println("OK, have a nice day.");
+                    System.exit(0);
+                }
 
-        String option = scanner.nextLine();
-        int optionNum = Integer.valueOf(option);
-
-        if (optionNum == 1) {
-            System.out.println("Your balance is $" + balance);
-        } else if (optionNum == 2) {
-            System.out.println("Withdrawal amount?");
-            String amount = scanner.nextLine();
-            int amountNum = Integer.valueOf(amount);
-
-            if (amountNum > balance) {
-                throw new Exception("Woah there Bill Gates! Please try again.");
-            } else {
-                System.out.println("Please take cash.");
-                System.out.println("Have a good day.");
             }
-        }
 
-        if (optionNum == 3) {
-            System.out.println("Thank you have a good day.");
+            double balance = accounts.get(name);
+
+            System.out.println("1. Check my balance");
+            System.out.println("2. Withdraw Funds");
+            System.out.println("3. Cancel");
+            System.out.println("4. Delete account");
+
+            String option = scanner.nextLine();
+            int optionNum = Integer.valueOf(option);
+
+            if (optionNum == 1) {
+                System.out.println("Your balance is $" + balance);
+            } else if (optionNum == 2) {
+                System.out.println("Withdrawal amount?");
+                String amount = scanner.nextLine();
+                double amountNum = Double.valueOf(amount);
+
+                if (amountNum > balance) {
+                    throw new Exception("Insufficient funds");
+                } else {
+                    balance = balance - amountNum;
+                    accounts.put(name, balance);
+                    System.out.println("Please take cash.");
+                    System.out.println("Have a good day.");
+                }
+            } else if(optionNum == 3) {
+                System.out.println("Thank you have a good day.");
+            } else if(optionNum == 4) {
+                accounts.remove(name);
+            }
         }
     }
 }
